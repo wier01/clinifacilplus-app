450da530e1c34f856948731babafdf2aaea26121
Felipe Wier <wier.felipe@gmail.com>
2026-02-07 15:26:07 -0300

    simplify metro config for vercel

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,6 @@
-const fs = require("fs");
-const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const projectRoot = __dirname;
-const cacheDir = path.join(projectRoot, ".cache", "nativewind");
-
-fs.mkdirSync(cacheDir, { recursive: true });
-process.env.RN_CSS_INTEROP_CACHE_DIR = cacheDir;
-
-const config = getDefaultConfig(projectRoot);
-config.watchFolders = [cacheDir];
+const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: "./global.css" });
