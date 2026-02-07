b4e19566a9c0ae7ca98a8f5ab13528ffcccf6ddf
Felipe Wier <wier.felipe@gmail.com>
2026-02-07 13:47:36 -0300

    fix nativewind css-interop cache for vercel

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,13 @@
-﻿const fs = require("fs");
-const path = require("path");
+﻿const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-// Force nativewind/css-interop cache inside the project so Metro can read it in CI.
-const interopCacheDir = path.join(__dirname, "node_modules", "react-native-css-interop", ".cache");
-const projectCacheDir = path.join(__dirname, ".cache", "nativewind");
+const projectRoot = __dirname;
+const cacheDir = path.join(projectRoot, ".cache", "nativewind");
 
-process.env.CSS_INTEROP_CACHE_DIR = interopCacheDir;
-fs.mkdirSync(interopCacheDir, { recursive: true });
-fs.mkdirSync(projectCacheDir, { recursive: true });
+process.env.RN_CSS_INTEROP_CACHE_DIR = cacheDir;
 
-const config = getDefaultConfig(__dirname);
-config.watchFolders = [path.join(__dirname, ".cache"), interopCacheDir];
+const config = getDefaultConfig(projectRoot);
+config.watchFolders = [cacheDir];
 
-module.exports = withNativeWind(config, {
-  input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
-});
+module.exports = withNativeWind(config, { input: "./global.css" });
