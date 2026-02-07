d00227e8a5552a19a973235ec3ec5c174b2e7099
Felipe Wier <wier.felipe@gmail.com>
2026-02-07 13:51:21 -0300

    ensure nativewind cache dir on vercel

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,12 @@
-﻿const path = require("path");
+﻿const fs = require("fs");
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const projectRoot = __dirname;
 const cacheDir = path.join(projectRoot, ".cache", "nativewind");
 
+fs.mkdirSync(cacheDir, { recursive: true });
 process.env.RN_CSS_INTEROP_CACHE_DIR = cacheDir;
 
 const config = getDefaultConfig(projectRoot);
