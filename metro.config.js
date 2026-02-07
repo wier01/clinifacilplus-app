f62011674f12650fb30398aee0b73b7acb86f2f2
Felipe Wier <wier.felipe@gmail.com>
2026-02-07 13:20:35 -0300

    fix nativewind cache for vercel

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,12 @@
+﻿const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+// Force nativewind cache inside the project so Metro can read it in CI.
+process.env.CSS_INTEROP_CACHE_DIR = path.join(__dirname, ".cache", "nativewind");
+
 const config = getDefaultConfig(__dirname);
+config.watchFolders = [path.join(__dirname, ".cache")];
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
