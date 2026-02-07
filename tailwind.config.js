395a8dbfe002858ee5c52832687cd144ef9ce91a
Felipe Wier <wier.felipe@gmail.com>
2026-02-07 09:26:17 -0300

    fix nativewind preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
-﻿module.exports = {
-  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-  theme: { extend: {} },
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require("nativewind/preset")],
+  content: [
+    "./app/**/*.{js,ts,tsx}",
+    "./components/**/*.{js,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
