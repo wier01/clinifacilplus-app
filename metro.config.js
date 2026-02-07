760e005a4ef7af3161e4ccdc0ec2b95f0ffa0d6b
Felipe Wier <wier.felipe@gmail.com>
2026-02-07 15:42:17 -0300

    simplify metro config one-liner

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,1 @@
-const expoMetroConfig = require("expo/metro-config");
-const nativewindMetro = require("nativewind/metro");
-
-const config = expoMetroConfig.getDefaultConfig(__dirname);
-
-module.exports = nativewindMetro.withNativeWind(config, {
-  input: "./global.css",
-});
+module.exports = require("expo/metro-config").getDefaultConfig(__dirname);
